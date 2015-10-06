35944bbecc818b21d81efdc954ad81b3423f1083
David Yang <daworld.ny@gmail.com>
2015-10-07 03:44:05 +0500

    Moved AboutBox to standalone repository

---
+++ Config/module.config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+return array(
+	'module' => array(
+		'url'	 => '/admin/module/about-box',
+		'icon'	 => '/module/AboutBox/Assets/aboutbox.png',
+		'name'	 => 'About box',
+		'order'	 => 5,
+		'description' => 'About box module allows to handle small info about you or your company on your blog'
+	)
+);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# AboutBox
-About box module
+AboutBox module
+===============
+
+AboutBox is a simple module to manage one box on your site. For details, please refer its documentation.
