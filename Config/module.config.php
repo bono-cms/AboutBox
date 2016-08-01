2d9d4dfee1310de8f33f745e66800b32afc6da45
David Yang <daworld.ny@gmail.com>
2016-08-01 22:26:46 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,20 +1,9 @@
 <?php
 
-/**
- * This file is part of the Bono CMS
- * 
- * Copyright (c) No Global State Lab
- * 
- * For the full copyright and license information, please view
- * the license file that was distributed with this source code.
- */
-
 return array(
-    'module' => array(
-        'route'  => 'AboutBox:Admin:Config@indexAction',
-        'icon'   => 'fa fa-hashtag fa-5x',
-        'name'   => 'About box',
-        'order'  => 5,
-        'description' => 'About box module allows to handle small info about you or your company on your blog'
-    )
+ 'route' => 'AboutBox:Admin:Config@indexAction',
+ 'icon' => 'fa fa-hashtag fa-5x',
+ 'name' => 'About box',
+ 'order' => 5,
+ 'description' => 'About box module allows to handle small info about you or your company on your blog'
 );
