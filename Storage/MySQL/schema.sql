1a84daa0aad74b62b2104028486b1c94f5bb5d37
David Yang <daworld.ny@gmail.com>
2017-05-15 18:51:31 +0500

    longtext instead of text

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS `bono_module_aboutbox`;
 
 CREATE TABLE `bono_module_aboutbox` (
 	
-	`content` TEXT NOT NULL COMMENT 'Content',
+	`content` LONGTEXT NOT NULL COMMENT 'Content',
 	`lang_id` INT NOT NULL COMMENT 'Associated language id'
 	
 ) DEFAULT CHARSET = UTF8;
