0eeaf2fe3b9bcff5bda738d5373339ab2bec7f76
dave-yang <daworld.ny@gmail.com>
2019-08-28 23:53:13 +0500

    Updated module definition

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,10 +1,22 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
- 'route' => 'AboutBox:Admin:Config@indexAction',
- 'icon' => 'fa fa-hashtag fa-5x',
- 'name' => 'AboutBox',
- 'caption' => 'About box',
- 'order' => 5,
- 'description' => 'About box module allows to handle small info about you or your company on your blog'
+    'caption' => 'About box',
+    'order' => 5,
+    'menu' => array(
+        'name' => 'AboutBox',
+        'icon' => 'fas fa-file-signature',
+        'items' => array(
+            array(
+                'route' => 'AboutBox:Admin:Config@indexAction',
+                'icon' => 'fa fa-hashtag fa-5x',
+                'name' => 'AboutBox',
+                'description' => 'About box module allows to handle small info about you or your company on your blog'
+            )
+        )
+    )
 );
